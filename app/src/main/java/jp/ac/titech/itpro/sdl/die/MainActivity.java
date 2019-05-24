509372d9f22b6e18ebd253d272e93f8ee2b87018
Jeshan Air <jasondangol>
2019-05-24 18:00:09 +0900

    Added optional automatic rotation of 3D object using Timer.

--- app/src/main/java/jp/ac/titech/itpro/sdl/die/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/die/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.titech.itpro.sdl.die;
 
 import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +11,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, Runnable {
     private final static String TAG = MainActivity.class.getSimpleName();
 
     private GLSurfaceView glView;
@@ -16,6 +22,14 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     private Cube cube;
     private Pyramid pyramid;
+    private Prism prism;
+
+    // for timer: automatic change of seek value, and thus rotation of object.
+    private final static long SPIN_REFRESH_PERIOD_MS = 33; //assuming 30fps
+    private final Timer timer = new Timer();
+    private Handler handler = new Handler();
+
+    SeekBar seekBarX, seekBarY, seekBarZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +38,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         setContentView(R.layout.activity_main);
 
         glView = findViewById(R.id.gl_view);
-        SeekBar seekBarX = findViewById(R.id.seekbar_x);
-        SeekBar seekBarY = findViewById(R.id.seekbar_y);
-        SeekBar seekBarZ = findViewById(R.id.seekbar_z);
+        seekBarX = findViewById(R.id.seekbar_x);
+        seekBarY = findViewById(R.id.seekbar_y);
+        seekBarZ = findViewById(R.id.seekbar_z);
         seekBarX.setMax(360);
         seekBarY.setMax(360);
         seekBarZ.setMax(360);
@@ -37,8 +51,17 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         renderer = new SimpleRenderer();
         cube = new Cube();
         pyramid = new Pyramid();
+        prism = new Prism();
         renderer.setObj(cube);
         glView.setRenderer(renderer);
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(MainActivity.this);
+            }
+        }, 500, SPIN_REFRESH_PERIOD_MS);
+
     }
 
     @Override
@@ -66,12 +89,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         public boolean onOptionsItemSelected(MenuItem item) {
         Log.d(TAG, "onOptionsItemSelected");
         switch (item.getItemId()) {
-        case R.id.menu_cube:
-            renderer.setObj(cube);
-            break;
-        case R.id.menu_pyramid:
-            renderer.setObj(pyramid);
-            break;
+            case R.id.menu_cube:
+                renderer.setObj(cube);
+                break;
+            case R.id.menu_pyramid:
+                renderer.setObj(pyramid);
+                break;
+            case R.id.menu_prism:
+                renderer.setObj(prism);
+                break;
         }
         return true;
     }
@@ -97,5 +123,17 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override // called by handler
+    public void run() {
+        int x = seekBarX.getProgress();
+        int y = seekBarY.getProgress();
+        int z = seekBarZ.getProgress();
+        seekBarX.setProgress((x + 2) % 360,true);
+        seekBarY.setProgress((y + 2) % 360,true);
+        seekBarZ.setProgress((z + 2) % 360,true);
+
     }
 }
--- app/src/main/java/jp/ac/titech/itpro/sdl/die/Prism.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/die/Prism.java
@@ -1,4 +1,74 @@
 package jp.ac.titech.itpro.sdl.die;
 
-public class Prism {
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class Prism implements Obj{
+
+    private final static float[] VERTICES = {
+            // bottom
+            -1, 0, -1,
+            1, 0, -1,
+            -1, 0, 1,
+            1, 0, 1,
+            // left
+            -1, 0, -1,
+            -1, 1, 0,
+            -1, 0, 1,
+            // right
+            1, 0, -1,
+            1, 1, 0,
+            1, 0, 1,
+            // back
+            -1, 0, -1,
+            -1, 1, 0,
+            1, 0, -1,
+            1, 1, 0,
+            // front
+            -1, 0, 1,
+            -1, 1, 0,
+            1, 0, 1,
+            1, 1, 0
+    };
+
+    private final FloatBuffer vbuf;
+
+    Prism(){
+        vbuf = ByteBuffer
+                .allocateDirect(VERTICES.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vbuf.put(VERTICES);
+        vbuf.position(0);
+    }
+
+    @Override
+    public void draw(GL10 gl) {
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vbuf);
+
+        // bottom
+        gl.glNormal3f(0, -1, 0);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+
+        // left
+        gl.glNormal3f(-1, 0, 0);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 4, 3);
+
+        // right
+        gl.glNormal3f(1, 0, 0);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 7, 3);
+
+        // back
+        gl.glNormal3f(0, 1, -1);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 10, 4);
+
+        // front
+        gl.glNormal3f(0, 1, 1);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 14, 4);
+    }
+
 }
